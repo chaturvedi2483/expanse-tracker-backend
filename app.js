4ad19f70b22ec8c2e4f536b8d4123ecc3b5eefbf
chaturvedi2483 <chaturvedi2483@gmail.com>
2026-01-12 15:33:04 +0530

    Fix backend server start and CORS

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 const express = require('express')
-const cors = require('cors');
-const { db } = require('./db/db');
-const {readdirSync} = require('fs')
-const app = express()
-
+const cors = require('cors')
+const { db } = require('./db/db')
+const { readdirSync } = require('fs')
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const app = express()
+const PORT = process.env.PORT || 5000
 
-//middlewares
+
 app.use(express.json())
-app.use(cors())
 
-//routes 
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://expanse-tracker-frontend-omega.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+)
+
+readdirSync('./routes').map((route) =>
+  app.use('/api/v1', require('./routes/' + route))
+)
+
 
 const server = () => {
-    db()
-    app.listen(PORT, () => {
-        console.log('listening to port:', PORT)
-    })
+  db()
+  app.listen(PORT, () => {
+    console.log(` Server running on port ${PORT}`)
+  })
 }
 
 server() 
